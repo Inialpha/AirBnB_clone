b1e1a21d9109082dbc46bfcf3b7c681792e4c753
Initech <inimfonebong001@gmail.com>
2023-08-07 11:20:08 -0700

    create the BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This is the base module which contain
+the BaseModel class inherited by all other major class in the project"""
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """BaseModel class serve as a base class for all class"""
+
+    def __init__(self):
+        """class constructor"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """prints the representation of the object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """update the updated_at instance attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """return the dictionary representation of an object"""
+
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        return my_dict
+
+
+"""base = BaseModel()
+print(base)
+print(type(base))
+print(base.id)
+print(base.created_at)
+print(base.updated_at)
+print(base.to_dict())"""
