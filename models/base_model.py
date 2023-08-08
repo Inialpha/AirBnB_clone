21de9ac690498c9a759c6bf0a4165c1e1911e32c
Rashnotech <rashnotech@gmail.com>
2023-08-08 07:16:50 +0000

    Implemented a method that recreates a basemodel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,14 @@ from uuid import uuid4
 class BaseModel:
     """BaseModel class serve as a base class for all class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """class constructor"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs) > 0:
+            self.__recreate_method(**kwargs)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """prints the representation of the object"""
@@ -32,3 +35,15 @@ class BaseModel:
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['created_at'] = self.created_at.isoformat()
         return my_dict
+
+    def __recreate_method(self, **kwargs):
+        """ a private method that recreates an instance with
+            dict representation
+        """
+        for attr, value in kwargs.items():
+            if attr == 'update_at' or attr == 'created_at':
+                setattr(self, attr, datetime.fromisoformat(value))
+            elif attr == '__class__':
+                pass
+            else:
+                setattr(self, attr, value)
