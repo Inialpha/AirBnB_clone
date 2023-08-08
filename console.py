abf404f938cac3d29f8848d7842ea3653b2a4b0e
Rashnotech <rashnotech@gmail.com>
2023-08-08 10:01:06 +0000

    Fixed: replace quoted name with just magic name

--- console.py
+++ console.py
@@ -9,19 +9,18 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, line):
-        """ a method that quite the console """
+        """ Quit command to exit the program """
         return True
-    """
-    def do_emptyline(self):
-        # a method that overrides when an emptyline and enter 
-        print('\n')
-        return cmd.Cmd.emptyline(self)
-    """
+
+    def emptyline(self):
+        """ a method that overrides when an emptyline when enter key
+            is press"""
+        pass
 
     def do_EOF(self, line):
         """ a method that Exit the console """
         return True
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
