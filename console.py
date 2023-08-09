9e5cf3893a0d8dde19a5ef834e0ad062f4977b5d
Initech <inimfonebong001@gmail.com>
2023-08-09 07:59:58 -0700

    Add the all and update commands

--- console.py
+++ console.py
@@ -69,6 +69,52 @@ class HBNBCommand(cmd.Cmd):
             my_dict = models.storage.all()
             if key in my_dict:
                 del (my_dict[key])
+                models.storage.save()
+            else:
+                print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """Prints all string representation
+        of all instances based or not on the class name."""
+        my_dict = models.storage.all()
+        all_list = []
+        if len(arg) == 0:
+            for key in my_dict:
+                all_list.append(str(my_dict[key]))
+        else:
+            if arg in self.all_classes:
+                for key, value in my_dict.items():
+                    if arg == value.to_dict()["__class__"]:
+                        all_list.append(str(value))
+            else:
+                print("** class doesn't exist **")
+                return
+        print(all_list)
+
+    def do_update(self, arg):
+        """Updates an instance based on the
+        class name and id by adding or updating attribute"""
+
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        elif args[0] in self.all_classes:
+            key = args[0] + "." + args[1]
+            my_dict = models.storage.all()
+            if key in my_dict:
+                if hasattr(my_dict[key], args[2]):
+                    a_type = type(getattr(my_dict[key], args[2]))
+                    args[3] = a_type(args[3])
+                setattr(my_dict[key], args[2], args[3])
+                models.storage.save()
             else:
                 print("** no instance found **")
         else:
