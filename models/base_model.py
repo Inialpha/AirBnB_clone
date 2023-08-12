e63a9e799927340af72f87d826b7696dc81f34c8
Initech <inimfonebong001@gmail.com>
2023-08-12 07:11:33 -0700

    Add more documentation for init method

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,10 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """class constructor with public instances id, created_at, updated_at"""
+        """class constructor with public instances:
+            id
+            created_at
+            updated_at"""
         if len(kwargs) > 0:
             self.__recreate_method(**kwargs)
         else:
