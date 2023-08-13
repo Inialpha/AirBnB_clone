7e6f2436f339703f8d57c1631a98678bc9aa31b7
Initech <inimfonebong001@gmail.com>
2023-08-13 06:58:24 -0700

    Add appropriate teat cases for user class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -7,6 +7,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.engine.file_storage import FileStorage
 import os
+from datetime import datetime
 
 
 class TestUser(unittest.TestCase):
@@ -20,13 +21,35 @@ class TestUser(unittest.TestCase):
         except FileNotFoundError:
             pass
 
-    def test_user_class_type(self):
+    def test_init(self):
         """ Testing class attribute data types """
+        self.assertTrue(isinstance(self.my_user, User))
+        self.assertTrue(isinstance(self.my_user, BaseModel))
+
+    def test_id(self):
+        """test user id"""
         self.assertIsNotNone(self.my_user.id)
-        self.assertTrue(type(self.my_user.first_name), str)
-        self.assertTrue(type(self.my_user.last_name), str)
-        self.assertTrue(type(self.my_user.email), str)
-        self.assertTrue(type(self.my_user.password), str)
+        self.assertIsInstance(self.my_user.id, str)
+
+    def test_created_at(self):
+        """test the created_at instance attribute"""
+        self.assertIsNotNone(self.my_user.created_at)
+        self.assertIsInstance(self.my_user.created_at, datetime)
+
+    def test_updated_at(self):
+        """test the updated_at attribute"""
+        self.assertIsNotNone(self.my_user.updated_at)
+        self.assertIsInstance(self.my_user.updated_at, datetime)
+
+    def test_to_dict(self):
+        """test the to_duct method"""
+        my_dict = self.my_user.to_dict()
+        self.assertIsInstance(my_dict, dict)
+
+    def test_save(self):
+        """test the save method"""
+        self.my_user.save()
+        self.assertTrue(os.path.exists("file.json"))
 
     def test_user_class_attribute(self):
         """ Testing class attribute if has attribute """
@@ -39,12 +62,6 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.my_user.email, 'airbnb@mail.com')
         self.assertEqual(self.my_user.password, 'root')
 
-    def test_user_inherits_base_class(self):
-        """ Testing User Class to know if it inherit base class """
-        self.assertTrue(isinstance(self.my_user, BaseModel))
-        self.assertFalse(isinstance(self.my_user, FileStorage))
-
-    def test_user_has_save_methods(self):
         """ Testing check if dictionary was create when save was called """
         self.my_user.save()
         self.assertIsNotNone(self.my_user.created_at)
