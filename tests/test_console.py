12357763d4440b7763f7e327d0b19b6d6b340efb
Rashnotech <rashnotech@gmail.com>
2023-08-15 00:25:21 +0000

    Tested the quit command in console

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,11 +4,28 @@
 
 from unittest.mock import patch
 from io import StringIO
+import unittest
 from console import HBNBCommand
 
-with patch('sys.stdout', new=StringIO()) as f:
-    HBNBCommand().onecmd("help show")
+class TestHBNBCommand(unittest.TestCase):
+    """Testing all the implemented functionality in console"""
+    
+    def setUp(self):
+        """Setting up the console"""
+        self.fake_stdout = StringIO()
+        self.patcher = patch('sys.stdout', new=self.fake_stdout)
+        self.patcher.start()
+
+    def test_quit_command(self):
+        """Test quit command"""
+        HBNBCommand().onecmd("quit")
+        output = self.fake_stdout.getvalue()
+        self.assertIsNotNone(output)
+
+    def tearDown(self):
+        """Tear down the console"""
+        self.patcher.stop()
 
 
 if __name__ == '__main__':
-    HBNBCommand().unittest()
+    unittest.main()
