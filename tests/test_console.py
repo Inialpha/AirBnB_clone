c72a3d932904017481d39f038dd1c05176cb80d8
Rashnotech <rashnotech@gmail.com>
2023-08-15 05:33:48 +0000

    Added tests on console

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,13 +20,49 @@ class TestHBNBCommand(unittest.TestCase):
         """Test quit command"""
         HBNBCommand().onecmd("quit")
         output = self.fake_stdout.getvalue()
-        assert "" in output
+        self.assertIsNotNone(output)
 
     def test_help_command(self):
         """ Test help command """
         HBNBCommand().onecmd("help")
         output = self.fake_stdout.getvalue()
         assert "Documented commands (type help <topic>):" in output
+    
+    def test_empty_line(self):
+        """ Test empty line """
+        HBNBCommand().onecmd(" ")
+        output = self.fake_stdout.getvalue()
+        self.assertIsNotNone(output)
+
+    def test_create_command(self):
+        """Test create command"""
+        HBNBCommand().onecmd("create BaseModel")
+        output = self.fake_stdout.getvalue()
+        self.assertIsNotNone(output)
+
+    def test_show_command(self):
+        """Test show command """
+        HBNBCommand().onecmd("show BaseModel")
+        output = self.fake_stdout.getvalue()
+        self.assertIsNotNone(output)
+
+    def test_destroy_command(self):
+        """Test all command"""
+        HBNBCommand().onecmd("destroy BaseModel")
+        output = self.fake_stdout.getvalue()
+        assert "** instance id missing **" in output
+
+    def test_all_command(self):
+        """Test all command"""
+        HBNBCommand().onecmd("all BaseModel")
+        output = self.fake_stdout.getvalue()
+        assert '[]' in output
+
+    def test_update_command(self):
+        """Test update command"""
+        HBNBCommand().onecmd("update BaseModel")
+        output = self.fake_stdout.getvalue()
+        assert "** instance id missing **" in output
 
     def tearDown(self):
         """Tear down the console"""
